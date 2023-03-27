81f76cc07608fc08195b53f9cd3d6873999965f0
CIC <carlosicg07@gmail.com>
2023-03-27 16:54:03 -0600

    Ejercicio 1 terminado, inicio del 2

--- JS-04 - Arrays and Loops/js/main.js
+++ JS-04 - Arrays and Loops/js/main.js
@@ -1,10 +1,14 @@
+console.log("-----PERSONAS EN LA FILA-----");
 let names=["Sofia", "David", "Juan"]; //fila original
 names[3]="Sara";
 names[4]="Agustín";  // se añaden 2 personas nuevas
-console.log(names.shift()); // la personma que sigue fue llamada
+names.shift(); // Atienden a la persona al frente de la linea
+names.splice(2, 0, 'Renata'); //Renata toma el lugar después de David
+names.push("Elena"); //Llega otra persona
 
 let personas=0;
-console.log("Lista de personas en la fila:");
 for (personas=0; personas<names.length; personas++){
     console.log(names[personas]);
 }
+
+console.log("-----PATERN EJ.2-----");
