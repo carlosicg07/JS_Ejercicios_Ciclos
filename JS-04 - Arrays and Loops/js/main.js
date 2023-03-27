fe0b514dc875a9bbb9e423d1647acba18c8f0689
CIC <carlosicg07@gmail.com>
2023-03-27 17:21:51 -0600

    Ejercicios 2 y 3 se inician y al 1 se le da mejor formato

--- JS-04 - Arrays and Loops/js/main.js
+++ JS-04 - Arrays and Loops/js/main.js
@@ -1,14 +1,36 @@
 console.log("-----PERSONAS EN LA FILA-----");
-let names=["Sofia", "David", "Juan"]; //fila original
-names[3]="Sara";
-names[4]="Agustín";  // se añaden 2 personas nuevas
-names.shift(); // Atienden a la persona al frente de la linea
-names.splice(2, 0, 'Renata'); //Renata toma el lugar después de David
-names.push("Elena"); //Llega otra persona
+console.log("PARTE 1");
+let names=["Sofia", "David", "Juan"]; //fila original.
+console.log(names);
+names.shift(); // Atienden a Sofia (se va).
+names.push("Sara");
+names.push("Agustín");  // se añaden 2 personas nuevas.
+console.log("PARTE 2");
+console.log(names);
+names.splice(1, 0, 'Renata'); //Renata toma el lugar después de David.
+names.push("Elena"); //Llega Elena al final de la fila.
+console.log("PARTE 3");
+console.log(names);
 
+
+console.log("-----PERSONAS EN LA FILA LISTA (EXTRA)-----");
 let personas=0;
 for (personas=0; personas<names.length; personas++){
     console.log(names[personas]);
 }
 
+
+/*
 console.log("-----PATERN EJ.2-----");
+let sign="*";
+for(sign="*"; sign<)
+
+
+
+console.log("-----LOOPS EJ.3-----");
+let xValue = 10;
+while (xValue < xValue.value){
+    console.log()
+}
+
+*/
