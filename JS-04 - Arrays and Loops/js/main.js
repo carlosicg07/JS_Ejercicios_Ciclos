f35e353dee3c86e081a4938f13e4c35ac9d0e80d
CIC <carlosicg07@gmail.com>
2023-03-27 19:46:03 -0600

    Ejercicio 2 terminado pero con dudas y 3 iniciado

--- JS-04 - Arrays and Loops/js/main.js
+++ JS-04 - Arrays and Loops/js/main.js
@@ -1,6 +1,6 @@
-console.log("-----PERSONAS EN LA FILA-----");
+console.log("-----------------------PERSONAS EN LA FILA-----------------------");
 console.log("PARTE 1");
-let names=["Sofia", "David", "Juan"]; //fila original.
+let names = ["Sofia", "David", "Juan"]; //fila original.
 console.log(names);
 names.shift(); // Atienden a Sofia (se va).
 names.push("Sara");
@@ -12,30 +12,70 @@ names.push("Elena"); //Llega Elena al final de la fila.
 console.log("PARTE 3");
 console.log(names);
 
-console.log("-----PERSONAS EN LA FILA LISTA (EXTRA)-----");
-let personas=0;
-for (personas=0; personas<names.length; personas++){
+console.log("-------PERSONAS EN LA FILA LISTA (EXTRA)-------");
+let personas = 0;
+for (personas = 0; personas < names.length; personas++) {
     console.log(names[personas]);
 }
 
-//--------------------------------------------------------------------
 
-console.log("-----PATERN EJ.2-----");
+//--------------------------------------------------------------------
+console.log("-----------------------EJ.2 PATERN-----------------------");
+
 var x, y, sign
-for (x=1; x<=6; x++){
-    for(y=1; y<x; y++){
-        sign=sign+("*");
+
+for (x = 0; x <= 5; x++) {
+    for (y = 0; y < x; y++) {
+        sign = sign + ("*");
     }
     console.log(sign);
-    sign='';
+    sign = '';
 }
 
-/*
+console.log("-------ENTERNDER FOR-------");
+for(let i = 0 ; i<9 ; i++){
+    console.log(i);
+}
+
+
 //--------------------------------------------------------------------
-console.log("-----LOOPS EJ.3-----");
-let xv = 10;
-while (xv < xValue.value){
-    console.log()
+console.log("-----------------------EJ.3 LOOPS-----------------------");
+console.log("-------PARTE 1-------");
+let xValue = 10;
+while (xValue > 0) {
+    xValue-=(0.5);
+    console.log(xValue);
 }
 
-*/
+console.log("-------PARTE 2-------");
+let number = 1
+while(number+2 < 100){
+    number+=(2);
+    console.log(number);
+}
+
+console.log("-------PARTE 3-------");
+let n = 10
+let z=0;
+while(z<n){
+    z++;
+    console.log([z])
+}
+
+console.log("-------PARTE 4-------");
+let N = 5
+let X = 0
+while(X<N){
+     
+    X++;
+    console.log(X);
+} 
+console.log("suma = "+ X)
+
+funtion sum(N,X){
+    if (X<N){
+
+    }
+    return 
+
+}
